2a3d1d97ad0615937196f15882fe8cf4654a2381
Jonathan DeCarlo <jddecarlo@gmail.com>
2019-12-01 22:44:58 -0500

    Added a little more code to BoardState. WIP.

--- src/Sudoku/BoardState.java
+++ src/Sudoku/BoardState.java
@@ -1,6 +1,9 @@
 package Sudoku;
 
-import java.security.InvalidParameterException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class BoardState {
     public BoardState() {
@@ -14,6 +17,28 @@ public class BoardState {
 
         for(int i = 0; i < 81; i++)
             this.board[i] = cellStates[i].clone();
+    }
+
+    public BoardState(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int index = 0;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                for (int i = 0; i < values.length; i++)
+                    this.board[index++].setValue(Integer.parseInt(values[i]));
+            }
+        }
+        catch(FileNotFoundException e) {
+            // TODO: Handle this.
+        }
+        catch(IOException e) {
+            // TODO: Handle this.
+        }
+    }
+
+    public boolean isBlankCell(int i) {
+        return this.board[i].isBlankCell();
     }
 
     private CellState[] board = new CellState[81];
