42bb0a14115dbe14d476dbbb7328d07f5244c13a
Jonathan DeCarlo <jddecarlo@gmail.com>
2019-12-02 14:25:09 -0500

    Added more code to BoardState. WIP.

--- src/Sudoku/BoardState.java
+++ src/Sudoku/BoardState.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BoardState {
     public BoardState() {
@@ -37,8 +39,79 @@ public class BoardState {
         }
     }
 
+    public static int getRowStartIndex(int i) {
+        return i / 9;
+    }
+
+    public static int getColumnStartIndex(int i) {
+        return i % 9;
+    }
+
+    public static int getSquareStartIndex(int i) {
+        return (BoardState.getSquareIndex(i) % 3) * 3 + (BoardState.getSquareIndex(i) / 3 * 27);
+    }
+
+    public static int getSquareIndex(int i) {
+        return BoardState.getColumnStartIndex(i) / 3 + 3 * (BoardState.getRowStartIndex(i) / 3);
+    }
+
     public boolean isBlankCell(int i) {
         return this.board[i].isBlankCell();
+    }
+
+    public Set<Integer> calculatePossibleValues(int i) {
+        Set<Integer> possibleValues = new HashSet<Integer>();
+        if (!this.board[i].isBlankCell()) {
+            possibleValues.add(this.board[i].getValue());
+            return possibleValues;
+        }
+
+        Set<Integer> relatedCellIndices = getRelatedCellIndices(i);
+        Set<Integer> existingValues = new HashSet<Integer>();
+        for (int index : relatedCellIndices) {
+            CellState cellState = this.board[index];
+            if (!cellState.isBlankCell())
+                existingValues.add(cellState.getValue());
+        }
+
+        possibleValues.add(1);
+        possibleValues.add(2);
+        possibleValues.add(3);
+        possibleValues.add(4);
+        possibleValues.add(5);
+        possibleValues.add(6);
+        possibleValues.add(7);
+        possibleValues.add(8);
+        possibleValues.add(9);
+
+        for (Integer existingValue : existingValues)
+            possibleValues.remove(existingValue);
+
+        return possibleValues;
+    }
+
+    public boolean isBoardComplete() {
+        for (CellState cellState : this.board) {
+            if (cellState.isBlankCell())
+                return false;
+        }
+
+        return true;
+    }
+
+    private Set<Integer> getRelatedCellIndices(int i) {
+        int rowStart = BoardState.getRowStartIndex(i);
+        int columnStart = BoardState.getColumnStartIndex(i);
+        int squareStart = BoardState.getSquareStartIndex(i);
+        Set<Integer> indices = new HashSet<Integer>();
+        for (int index = 0; i < 9; i++)
+        {
+            indices.add(rowStart * 9 + index);
+            indices.add(index * 9 + columnStart);
+            indices.add(squareStart + (index % 3) + ((index / 3) * 9));
+        }
+
+        return indices;
     }
 
     private CellState[] board = new CellState[81];
